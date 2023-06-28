dcae6eeb820579594b5941a83a594bf0e73835f5
Paulius Birstonas <33183201+birstonas@users.noreply.github.com>
2023-06-28 15:42:31 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -13,14 +13,14 @@ mail = Mail()
 #nustatymai
 def create_app(debug=False):
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'B8D96DBA7EFEDCF311ED14DFDF3BA'
+    app.config['SECRET_KEY'] = '##'
     app.config['MQTT_BROKER_URL'] = 'localhost'
     app.config['MQTT_USERNAME'] = 'paulius'
-    app.config['MQTT_PASSWORD'] = 'slaptas'
+    app.config['MQTT_PASSWORD'] = '##'
     app.config['MQTT_BROKER_PORT'] = 1883
     app.config['MQTT_TLS_ENABLED'] = False
     app.config['TEMPLATES_AUTO_RELOAD'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Slapukas1@flask.cwujnebpbug4.eu-north-1.rds.amazonaws.com/flask'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://'
     app.config['SQLALCHEY_TRACK_MODIFICATIONS'] = False
 #el.pašto nustatymai
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
